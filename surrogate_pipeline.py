954be2bdf4e93ab4abb9ba0e3d76a8a788f70744
Daniel Harley Jordan <daniel.harley.jordan@outlook.com>
2024-05-02 17:00:20 +0300

    save

---
+++ surrogate_pipeline.py
@@ -0,0 +1,10 @@
+#imports
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd(),'enchanted-surrogates','src'))
+sys.path.append(os.path.join(os.getcwd(),'GENE_UQ'))
+from parsers import GENEparser
+from GENE_UQ.Samplers import uniform
+
+if __name__ == '__main__':
+    print('hello')
