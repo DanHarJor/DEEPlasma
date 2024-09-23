b81677d673df66d038fc5ae081531e81c1b62ed8
danharjor <daniel.harley.jordan@outlook.com>
2024-09-23 21:16:57 +0300

    update after implmenting simtimelim and trying to make example runs

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config():
         #The parsers main function is write_input_file
         # wite_input_file takes a parameters file from base_params_path and a set of points in the form of a dict {param1:[point1,point2...], param2:[point1,point2...]...} 
         #  It will then create a parameters file that can scan over all the points.
-        self.base_params_path = "/home/djdaniel/DEEPlasma/parameters_base_dp" 
+        self.base_params_path = "/home/djdaniel/DEEPlasma/parameters_base_dependancies_fixed_dp" 
         self.remote_save_base_dir=f'/scratch/project_462000451/gene_out/gene_auto/'
         self.save_dir = "/home/djdaniel/DEEPlasma/temp/"
 
@@ -13,7 +13,7 @@ class Config():
         #The Runner is responsible for actually running a parameters file on lumi. Its main function is code_run.
         # code_run will take the set of points named samples and parse them into a parameters file.
         #  It then uses ssh to run GENE with this parametres file and a passed sbatch script.
-        self.host = 'lumi1' #needs to be configured in /home/<user>/.ssh/config
+        self.host = 'lumi' #needs to be configured in /home/<user>/.ssh/config
         self.sbatch_base_path = "/home/djdaniel/DEEPlasma/sbatch_base_dp" 
         # single_run_timelim = 200 # a guess for the number of seconds it takes to run one sample.
         self.remote_run_dir = '/project/project_462000451/gene/'
